603ddedfe871bd25e159220e3ff0622bf64036fc
Elayarajathiru <50883553+Elayarajathiru@users.noreply.github.com>
2023-06-13 18:08:25 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class baseClass
+
+{
+    public void show()
+    {
+        Console.WriteLine("Base class");
+    }
+}
+
+class derivedClass : baseClass
+{
+
+    // overriding in derived class
+    new public void show()
+    {
+        Console.WriteLine("Derived class");
+    }
+}
+
+class GFG
+{
+    public static void Main()
+    {
+        baseClass obj = new baseClass();
+        obj.show();
+        obj = new derivedClass();
+        obj.show();
+
+        derivedClass d = new derivedClass();
+        d.show();
+        Console.ReadLine();
+
+    }
+}
